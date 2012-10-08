75e9054af5fd2f23091fa5423d8931076a5e69bb
Justin Tim <insomniac.robot@gmail.com>
2012-10-08 10:37:03 +0200

    determinant test

--- tests/src/com/insomniacmath/tests/determinant/Determinant3x3Test.java
+++ tests/src/com/insomniacmath/tests/determinant/Determinant3x3Test.java
@@ -11,20 +11,8 @@ import junit.framework.Assert;
  * by insomniac and angryded
  * for their purposes
  */
-public class DeterminantTest extends
+public class Determinant3x3Test extends
         GeneralTest {
-
-    public void testDetermin2x2() throws Exception {
-
-        enterEditByXY(0, 0, 2);
-        enterEditByXY(1, 0, 5);
-        enterEditByXY(0, 1, 8);
-        enterEditByXY(1, 1, 12);
-        solo.clickOnMenuItem("Determinant");
-        solo.sleep(10);
-        TextView result = (TextView) solo.getView(RESULT_ID);
-        Assert.assertEquals(result.getText(), "Determinant = " + "-16");
-    }
 
     public void testDetermin3x3() throws Exception {
 
