6fdb68b00551e71d35f00b510aa84a2d69c78fe8
insomniac <insomniac.robot@gmail.com>
2013-03-01 19:24:22 +0200

    start inverse

--- src/com/insomniacmath/Animations/InverseAnimation.java
+++ src/com/insomniacmath/Animations/InverseAnimation.java
@@ -2,13 +2,19 @@ package com.insomniacmath.Animations;
 
 
 import android.app.Activity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.UnderlineSpan;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.insomniacmath.math.Fraction;
 import com.insomniacmath.math.MatrixModel;
 import com.insomniacmath.math.MatrixUtils;
 import com.insomniacmath.ui.MatrixView;
 import com.insomniacmath.ui.MinorSolvationView;
+import com.insomniacmath.ui.roboto.TextViewRoboto;
 
 public class InverseAnimation extends Animation {
 
@@ -22,13 +28,33 @@ public class InverseAnimation extends Animation {
 
     @Override
     public void animate() {
+
+        MatrixModel wholemodel = new MatrixModel(mW1.model.mFrac);
+        final TextView det = new TextView(solvation.getContext());
+        det.setGravity(Gravity.CENTER);
+        det.setTextSize(18);
+        det.setTextColor(0xffffffff);
+        det.setText("det = " + MatrixUtils.determin(wholemodel.mFrac).toString());
+        ((Activity) solvation.getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                solvation.addView(det, new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+        });
+        w();
+
         for (int i = 0; i < mW1.model.rows; i++) {
             for (int j = 0; j < mW1.model.columns; j++) {
                 Fraction[][] fractions = MatrixUtils.removeRowAndColumn(mW1.model.mFrac, i, j);
                 MatrixModel model = new MatrixModel(fractions);
                 final MinorSolvationView minor = new MinorSolvationView(solvation.getContext(), model);
-                minor.setPrefix("M(" + i + ")(" + j + ") = det");
-                minor.setPostfix(" =");
+                String prefix1 = "M" + i + j + "=det";
+                SpannableString ss = new SpannableString(prefix1);
+                ss.setSpan(new UnderlineSpan(), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                minor.setPrefix(prefix1);
+
+                minor.setPostfix("=" + MatrixUtils.determin(model.mFrac).toString());
                 ((Activity) solvation.getContext()).runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
--- src/com/insomniacmath/MatrixActivity.java
+++ src/com/insomniacmath/MatrixActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import com.insomniacmath.etc.Utils;
 
 public class MatrixActivity extends Activity implements Constants {
+
     private static final int MENU_EDIT = 3;
     private static final int MENU_DELETE = 4;
+
     /**
      * Called when the activity is first created.
      */
+
     Controller controller;
     LinearLayout mainView;
 
@@ -20,28 +24,29 @@ public class MatrixActivity extends Activity implements Constants {
         super.onCreate(savedInstanceState);
 
         Utils.resolvePlatform(this);
-
+        ScrollView scrollView = new ScrollView(this);
         mainView = new LinearLayout(this);
         mainView.setBackgroundColor(0xff000000);
         mainView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT));
         mainView.setOrientation(LinearLayout.VERTICAL);
+        scrollView.addView(mainView);
         controller = new Controller(this, mainView);
-        setContentView(mainView);
+        setContentView(scrollView);
 
     }
 
-//    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v,
-//                                    ContextMenu.ContextMenuInfo menuInfo) {
-//        menu.add(Menu.NONE, MENU_EDIT, 0, R.string.determinant);
-//        menu.add(Menu.NONE, MENU_DELETE, 1, R.string.multiply);
-//    }
-//
-//    @Override
-//    public boolean onContextItemSelected(MenuItem item) {
-//        controller.onMenu(item.getItemId());
-//        return true;
-//    }
+    //    @Override
+    //    public void onCreateContextMenu(ContextMenu menu, View v,
+    //                                    ContextMenu.ContextMenuInfo menuInfo) {
+    //        menu.add(Menu.NONE, MENU_EDIT, 0, R.string.determinant);
+    //        menu.add(Menu.NONE, MENU_DELETE, 1, R.string.multiply);
+    //    }
+    //
+    //    @Override
+    //    public boolean onContextItemSelected(MenuItem item) {
+    //        controller.onMenu(item.getItemId());
+    //        return true;
+    //    }
 
     @Override
     public void onBackPressed() {
